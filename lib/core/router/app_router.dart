da5be9a96832e2003b26f2348ef513568614bde7
Matar Ndiaye <ndiayemtr@gmail.com>
2026-09-06 09:02:42 +0000

    Navigation Appliances

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:go_router/go_router.dart';
 
 import '../../features/appliances/presentation/pages/appliance_form_page.dart';
+import '../../features/appliances/presentation/pages/appliances_page.dart';
 import '../../features/dashboard/presentation/pages/dashboard_page.dart';
 
 final GoRouter appRouter = GoRouter(
@@ -10,6 +11,10 @@ final GoRouter appRouter = GoRouter(
     GoRoute(
       path: '/appliances/add',
       builder: (context, state) => const ApplianceFormPage(),
+    ),
+    GoRoute(
+      path: '/appliances',
+      builder: (context, state) => const AppliancesPage(),
     ),
   ],
 );
